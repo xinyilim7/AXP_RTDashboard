ccd900b4af8170e802d49c203520d24f8ec990ef
xinyilim7 <xinyi.lim@axaipay.com>
2025-12-10 13:24:35 +0800

    updated Transaction id variable

--- DashboardBackend/src/main/java/com/example/dashboardbackend/Transaction.java
+++ DashboardBackend/src/main/java/com/example/dashboardbackend/Transaction.java
@@ -12,58 +12,11 @@ public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto increment ID
-    private Long id;
+    private String id;
     private Double amount;
     private String status;
     private String merchant;
     private String category;
     private LocalDateTime timestamp;
 
-    public Long getId() {
-        return id;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public String getMerchant() {
-        return merchant;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setMerchant(String merchant) {
-        this.merchant = merchant;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
 }
